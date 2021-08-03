78f2fdd54c7030011928520400d7d64795e24878
Deven Clingman <dclingy@gmail.com>
2021-08-02 21:00:31 -0600

    moduralize components

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,14 @@
+@props([
+    'trigger'
+])
+
+<div 
+    x-show="{{ $trigger }}"
+    x-on:click.self="{{ $trigger }} = false"
+    x-on:keydown.escape.window="{{ $trigger }} = false" 
+    class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+>
+    <div {{ $attributes->merge(['class' => 'm-auto bg-gray-200 shadow-2xl rounded-xl p-8']) }}>
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,8 +1,8 @@
 
 <div
     x-data="{
-        modalOpen: true,
-        showSuccess: true,
+        modalOpen: false,
+        showSuccess: false,
     }"
     class="flex flex-col bg-indigo-900 w-full h-screen"
 >
@@ -42,53 +42,40 @@
         </div>
     </div>
 
-    <div 
-    x-show="modalOpen"
-    x-on:click.self="modalOpen = false"
-    x-on:keydown.escape.window="modalOpen = false" 
-    class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
-    >
-        <div class="m-auto bg-pink-500 shadow-2xl rounded-xl p-8">
-            <p class="text-white text-5xl font-extrabold text-center">Lets do it!</p>
-            <form 
-                class="flex flex-col items-center p-24"
-                wire:submit.prevent="subscribe"  
-            >
-                <x-input
-                    class="px-5 py-3 w-80 border border-blue-400"
-                    type="email"
-                    name="name"
-                    placeholder="Email address"
-                    wire:model="email"
-                ></x-input>
-                <span class="text-gray-100 text-xs">
-                    We will send you a confirmation email.
-                </span>
-                <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center hover:bg-blue-600">
-                    Get In
-                </x-button>
-            </form>
-        </div>
-    </div>
+    
 
-    <div 
-    x-show="showSuccess"
-    x-on:click="showSuccess = false"
-    x-on:keydown.escape.window="showSuccess = false" 
-    class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
-    >
-        <div class="m-auto bg-green-500 shadow-2xl rounded-xl p-8">
-            <p class="animate-pulse text-white text-9xl font-extrabold text-center">
-                &check;
-            </p>
-            <p class="animate-pulse text-white text-5xl font-extrabold text-center mt-16">
-                Great!
-            </p>
-            <p class="text-white text-3xl font-bold text-center">
-                See you in your inbox
-            </p>
-        </div>
-    </div>
+    <x-modal class="bg-pink-500" trigger="modalOpen">
+        <p class="text-white text-5xl font-extrabold text-center">Lets do it!</p>
+        <form 
+            class="flex flex-col items-center p-24"
+            wire:submit.prevent="subscribe"  
+        >
+            <x-input
+                class="px-5 py-3 w-80 border border-blue-400"
+                type="email"
+                name="name"
+                placeholder="Email address"
+                wire:model="email"
+            ></x-input>
+            <span class="text-gray-100 text-xs">
+                We will send you a confirmation email.
+            </span>
+            <x-button class="px-5 py-3 mt-5 w-80 bg-blue-500 justify-center hover:bg-blue-600">
+                Get In
+            </x-button>
+        </form>
+    </x-modal>
 
+    <x-modal class="bg-green-500" trigger="showSuccess">
+        <p class="animate-pulse text-white text-9xl font-extrabold text-center">
+            &check;
+        </p>
+        <p class="animate-pulse text-white text-5xl font-extrabold text-center mt-16">
+            Great!
+        </p>
+        <p class="text-white text-3xl font-bold text-center">
+            See you in your inbox
+        </p>
+    </x-modal>
 </div>
 
